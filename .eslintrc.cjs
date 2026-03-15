54a4703265ef4023d70f444a9ec72cb4342dca56
hassennachar <hnachar@gmail.com>
2026-03-15 19:32:04 +0100

    feat: project base

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/multi-word-component-names': 'off',
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, autem?</p>
+</template>
